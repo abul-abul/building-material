c4dfe37020d40a46e4ff858709c393f9d592b184
Albert <abuladamyan@gmail.com>
2019-05-13 16:48:11 +0400

    a

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -5,18 +5,21 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
+class MainController extends Controller
 {
-//    /**
-//     * @Route("/", name="homepage")
-//     */
-//    public function indexAction(Request $request)
-//    {
-//
-//        // replace this example code with whatever you need
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function homeAction(Request $request)
+    {
+//dump(2);die;
+return [];
+        // replace this example code with whatever you need
 //        return $this->render('default/index.html.twig', [
 //            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
 //        ]);
-//    }
+    }
 }
